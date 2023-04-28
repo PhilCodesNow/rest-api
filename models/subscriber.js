866b552a37d140e2fb967d7f87bd60fb1b023140
PhilCodesNow <philipmayojr@outlook.com>
2023-04-28 18:55:26 -0400

    saving

---
+++ models/subscriber.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+
+const subscriberSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    }, 
+    subscribedTo: {
+        type: String,
+        required: true,
+    }, 
+    subscribeDate: {
+        type: Date,
+        required: true,
+        default: Date.now
+    }
+})
+
+
+
+
+
+
+module.exports = mongoose.model('Subscriber', subscriberSchema)
+
+
+
+
+
+
+
