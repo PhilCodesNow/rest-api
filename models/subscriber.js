dd25268e9772cad1ce1151295b8aa5b290b19e91
PhilCodesNow <philipmayojr@outlook.com>
2023-04-29 12:39:34 -0400

    finished tutorial

--- models/subscriber.js
+++ models/subscriber.js
@@ -6,7 +6,7 @@ const subscriberSchema = new mongoose.Schema({
         type: String,
         required: true,
     }, 
-    subscribedTo: {
+    subscribedToChannel: {
         type: String,
         required: true,
     }, 
